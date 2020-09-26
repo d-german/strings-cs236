902d754eef3d4b2da2eb19c1dbbd27456657919a
Damon German <damon.german@hyland.com>
2020-09-26 14:03:47 -0500

    made some changes

--- StringTests/StringTests/InClassLab.cs
+++ StringTests/StringTests/InClassLab.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Text;
+using NUnit.Framework;
 
 namespace StringTests
 {
@@ -14,7 +15,15 @@ namespace StringTests
     public class InClassLabTests
     {
         private const string ExpectedValue = "12345 College Blvd, Overland Park, KS 66210";
-        private Address _address;
+
+        private readonly Address _address = new Address
+        {
+            City = "Overland Park",
+            State = "KS",
+            StreetName = "College Blvd",
+            StreetNumber = "12345",
+            ZipCode = "66210"
+        };
 
         [Test]
         public void PlusOverloadOperatorTest()
@@ -32,15 +41,43 @@ namespace StringTests
             Assert.AreEqual(ExpectedValue, actualAddress);
         }
 
-        // string.Concat()
+        [Test]
+        public void ConcatTest()
+        {
+            var actualAddress = string.Concat(_address.StreetNumber, " ", _address.StreetName, ", ", _address.City, ", ", _address.State, " ", _address.ZipCode);
+            Assert.AreEqual(ExpectedValue, actualAddress);
+        }
 
+        [Test]
+        public void FormatTest()
+        {
+            var actualAddress = string.Format("{0} {1}, {2}, {3} {4}", _address.StreetNumber, _address.StreetName, _address.City, _address.State, _address.ZipCode);
+            Assert.AreEqual(ExpectedValue, actualAddress);
+        }
 
-        // string.Format()
-        
+        [Test]
+        public void StrungBuilderTest()
+        {
+            var buffer = new StringBuilder("");
+            var actualAddress = buffer
+                .Append(_address.StreetNumber)
+                .Append(" ")
+                .Append(_address.StreetName)
+                .Append(", ")
+                .Append(_address.City)
+                .Append(", ")
+                .Append(_address.State)
+                .Append(" ")
+                .Append(_address.ZipCode);
 
-        // StringBuilder class
-        
+            Assert.AreEqual(ExpectedValue, actualAddress.ToString());
+        }
 
-        //string interperlation $""
+        [Test]
+        public void InterpolationTest()
+        {
+            var actualAddress = $"{_address.StreetNumber} {_address.StreetName}, {_address.City}, {_address.State} {_address.ZipCode}";
+            Assert.AreEqual(ExpectedValue, actualAddress);
+        }
     }
 }
